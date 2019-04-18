8ff54ce48bafa5a3e6059af342d5d5b53013e7aa
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-04-18 00:13:50 -0400

    Import scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import L from "leaflet";
 import { Icon } from "./Icon";
 import { VectorMarkers } from "./VectorMarkers";
+import "./leaflet-vector-markers.scss";
 
 L.VectorMarkers = VectorMarkers;
 
