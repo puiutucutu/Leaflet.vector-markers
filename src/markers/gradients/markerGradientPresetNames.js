1e787a69aebcecdd6238fd7e673717780a967ea2
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-04-21 00:17:10 -0400

    Added more sample gradients

--- src/markers/gradients/markerGradientPresetNames.js
+++ src/markers/gradients/markerGradientPresetNames.js
@@ -27,10 +27,23 @@ const markerGradientPresetNames = {
     top: [49, 138, 176],
     bottom: [67, 180, 24]
   },
+  darkOcean: {
+    top: [66, 134, 244],
+    bottom: [55, 59, 68]
+  },
   red: {
     top: [255, 119, 43],
     bottom: [211, 60, 4]
+  },
+  purple: {
+    top: [234, 175, 200],
+    bottom: [101, 78, 163]
+  },
+  orange: {
+    top: [245, 175, 25],
+    bottom: [241, 39, 17]
   }
 };
 
 export { markerGradientPresetNames };
+
