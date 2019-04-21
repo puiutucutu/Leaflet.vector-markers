be59d15d930d56a0803d910acc46c933cf931702
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-04-20 21:58:06 -0400

    Extracted fn

--- src/markers/gradients/index.js
+++ src/markers/gradients/index.js
@@ -1,8 +1,10 @@
+import { doesSuppliedGradientNameExist } from "./doesSuppliedGradientNameExist";
 import { getAvailableGradientPresetNames } from "./getAvailableGradientPresetNames";
 import { getGradientPreset } from "./getGradientPreset";
 import { markerGradientPresetNames } from "./markerGradientPresetNames";
 
 export {
+  doesSuppliedGradientNameExist,
   getAvailableGradientPresetNames,
   getGradientPreset,
   markerGradientPresetNames
